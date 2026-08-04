96eef58c33056372f0d73eec295adf35f37bdc9c
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-08-04 15:32:28 +0200

    v

--- src/test/java/day03_implicitlywait_junit/C02_JunitFirstTest.java
+++ src/test/java/day03_implicitlywait_junit/C02_JunitFirstTest.java
@@ -5,9 +5,19 @@ import org.junit.jupiter.api.Test;
 public class C02_JunitFirstTest {
 
     @Test
-    public void test(){
+    public void test01(){
 
     }
 
-    
+    @Test
+    void test02() {
+
+    }
+
+    @Test
+    void test03() {
+
+    }
+
+
 }
