5fe14ffe3361b8ee1a04ba54079ab32bfc186899
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-08-04 15:41:27 +0200

    v

--- src/test/java/day03_implicitlywait_junit/C02_JunitFirstTest.java
+++ src/test/java/day03_implicitlywait_junit/C02_JunitFirstTest.java
@@ -1,23 +1,43 @@
 package day03_implicitlywait_junit;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
 
 public class C02_JunitFirstTest {
 
     @Test
     public void test01(){
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://techproeducation.com");
+        driver.close();
     }
 
     @Test
     void test02() {
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://techproeducation.com");
+        driver.close();
     }
 
     @Test
     void test03() {
-
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get("https://techproeducation.com");
+        driver.close();
     }
+
+
+
+
 
 
 }
