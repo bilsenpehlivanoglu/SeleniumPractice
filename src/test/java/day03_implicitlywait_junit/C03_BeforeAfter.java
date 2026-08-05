75b0c5891aeb4ea75e951b0b8ea07d2a1ccdda97
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-08-05 12:05:28 +0200

    v

---
+++ src/test/java/day03_implicitlywait_junit/C03_BeforeAfter.java
@@ -0,0 +1,37 @@
+package day03_implicitlywait_junit;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class C03_BeforeAfter {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println(" @BeforeAll tum test methodlarindan once 1 kere calisir");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("@BeforeEach her bir test methodlarindan once 1 kere calisir");
+    }
+
+    @Test
+    void test01() {
+        System.out.println("test01 calisti");
+    }
+    @Test
+    void test02() {
+        System.out.println("test02 calisti");
+    }
+    @Test
+    void test03() {
+        System.out.println("test03 calisti");
+    }
+
+    @AfterEach
+    void tearDown() {
+
+    }
+}
