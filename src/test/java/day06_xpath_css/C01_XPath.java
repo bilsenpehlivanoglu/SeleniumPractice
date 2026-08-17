2583bd4e1b8f4a021f105b6ae82392c546520b0d
bilsenpehlivanoglu <bilsensaturnus@gmail.com>
2026-08-17 15:26:55 +0200

    v

---
+++ src/test/java/day06_xpath_css/C01_XPath.java
@@ -0,0 +1,20 @@
+package day06_xpath_css;
+
+public class C01_XPath {
+
+    /*
+    / -> bir alt childi secer
+    // -> sayfadaki herhangi bir webelementi secer //div
+    @ -> bir webelementin attribute unu secmek icin kullanilir
+    [] -> Index veya kosul belirtmek icin kullanilir
+    * -> All farketmeksizin sec demektir
+
+    SYNTAX
+    //tagname [ @attributeName = 'attributeValue' ]
+    //*       [ @     *        = 'attributeValue' ]
+    (//tagname [ @attributeName = 'attributeValue' ]) [index]
+
+     */
+
+
+}
